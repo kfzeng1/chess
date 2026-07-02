625b3878b623c7ee9a1ca5c69cc56882d092e551
kfzeng1 <3886821565@qq.com>
2026-07-02 20:25:49 +0800

    Let updated service worker take control immediately

--- web/frontend/service-worker.js
+++ web/frontend/service-worker.js
@@ -12,13 +12,14 @@ const STATIC_ASSETS = [
 
 self.addEventListener("install", (event) => {
   event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(STATIC_ASSETS)));
+  self.skipWaiting();
 });
 
 self.addEventListener("activate", (event) => {
   event.waitUntil(
     caches.keys().then((keys) => Promise.all(
       keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
-    ))
+    )).then(() => self.clients.claim())
   );
 });
 
